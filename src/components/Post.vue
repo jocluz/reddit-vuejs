1a3c535ec965006bd45b64614799179585e10451
Josefina Espinosa <joc.luz@gmail.com>
2019-10-11 16:03:05 -0300

    Added fontawesome icons

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="post" @click='read'>
     <div v-if='!model.visited' class='post__unread'></div>
+    <i class='post__arrow fas fa-chevron-right'></i>
     <div class='post__header'>
       <div>
         <span class='author'>{{model.author}}</span>
-        <span class='created'>{{model.created}}</span>
+        <span class='created'>{{model.created_utc}}</span>
       </div>
-      <button @click='dismiss'>X</button>
+      <v-btn  text ligth @click='dismiss'>
+        <i class='fas fa-trash'></i>
+      </v-btn>
     </div>
     <div class='post__content'>
       <img v-if='model.thumbnail' :src='model.thumbnail'/>
@@ -38,17 +41,22 @@ export default {
 
 <style scoped lang="scss">
 .post {
+  --common-padding: 30px;
+  --comments-number-color: #f92a75;
+  --unread: #39c7c3;
+
   padding: 20px;
   position: relative;
   cursor: pointer;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
 
   &__unread {
     border-radius: 50%;
-    background: #39c7c3;
+    background: var(--unread);
     width: 15px;
     height: 15px;
     position: absolute;
-    top: 25px;
+    top: 30px;
   }
 
   &__arrow {
@@ -64,7 +72,7 @@ export default {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding-left: 30px;
+    padding-left: var(--common-padding);
 
     .author {
       font-size: 20px;
@@ -80,7 +88,7 @@ export default {
   &__content {
     display: flex;
     align-items: center;
-    padding: 10px 30px;
+    padding: 10px var(--common-padding);
 
     .title {
       padding-left: 10px;
@@ -89,7 +97,7 @@ export default {
 
   &__comments {
     font-size: 14px;
-    color: #f92a75;
+    color: var(--comments-number-color);
   }
 }
 </style>
